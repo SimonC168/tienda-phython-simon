1fcb8096f0a2eb7b4e9cf1665850f7e56f9b37b3
Juan Esteban Hoyos <jhoyoses@cesde.net>
2025-03-04 18:51:51 -0500

    feat se crea main con opciones

---
+++ mainh.py
@@ -0,0 +1,12 @@
+nombreVendedor=None
+producto=[]
+productos={}
+
+opcion=100
+
+print("mercado")
+print("1. agregar producto")
+print("preciona 5 para salir")
+while opcion!=5:
+    opcion=int(input("ingrese una opcion:"))
+    
