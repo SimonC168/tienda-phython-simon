f759949ac5ea8b4c9603973a6249ef4f32d5f47f
Juan Esteban Hoyos <jhoyoses@cesde.net>
2025-03-11 21:00:08 -0500

    feat se agrego rutina

--- mainh.py
+++ mainh.py
@@ -1,40 +1,55 @@
-nombreVendedor=None
+nombreVendedor=None 
 productos=[]
 producto={}
 
 opcion=100
 
-print("mercado")
-print("++++++++++++++++++++++++")
-print("1. agregar producto")
-print("2. ver lista de mercado")
-print("3. editar producto de la lista")
-print("4. eliminar producto de la lista")
-print("preciona 5 para salir")
-while opcion!=5:
-    opcion=int(input("ingrese una opcion:"))
-    if opcion==1:
-        print("bienvenido a la creacion de tu lista de peoductos")
-
-        #creaando clave valores de un diccionario
+print("Mercado")
+print("********")
+print("1. Crear lista mercado")
+print("2. Ver Lista de mercado")
+print("3. Editar producto de la lista")
+print("4. Retirar producto de la lista")
+print("Presiona 5 para salir")
+while opcion != 5:
+    opcion=int(input("Digita una opcion: "))
+    if opcion == 1:
+        print("Bienvenido a la creacion de tu lista de mercado")
+        
+        #creando claves y valores de un diccionario
         producto["id"]=5
-        producto["nombre"]=input("digite el nombre del producto:  ")
-        producto["precio"]=int(input("digita el precio del producto:  "))
-        producto["cantidad"]=input("digite la cantidad del producto:   ")
-        producto["presentacion"]=input("cual presentacion llevara:  ")
-
+        producto["nombre"]=input("Digita el nombre del producto: ")
+        producto["precio"]=int(input("Digita el precio del producto: "))
+        producto["cantidad"]=int(input("Cuantos elementos de este producto vas a llevar: "))
+        producto["presentacion"]=input("Cual presentacion llevaras? ")
+        
         #mostrando mi diccionario
-        print(producto)
-
-        #pablando una lista 
+        #print(producto)
+        
+        #poblando una lista (agrgando elementos a una lista)
         productos.append(producto)
         print(productos)
-    elif opcion == 2:
-        print("estoy en la dos")
-    elif opcion == 3:
-        print("estoy en la tres")
-    elif opcion==4:
-        print("estoy en la cuatro")
-    else:
-        print("no hay opcion")
         
+        
+        
+    elif opcion==2:
+        for productosSeleccionados in productos:
+            print(productosSeleccionados["nombre"])
+    elif opcion==3:
+        #0. preguntar a quien quieres editar
+        productoCambio=int(input("dijite el id del produnto  a cambiar"))
+        #1 encontrar el elemento
+        for productoBuscado in producto:
+            if productoBuscado["id"]==productoCambio:
+                print(" LO ENCONTRE")
+            else:
+                print("NO LO ENCONTRE")
+                    
+
+        #2 selecciono el elemento
+        #3 acedo a las propiedades o otributos
+        
+    elif opcion==4:
+        print("estoy en la 4")
+    else:
+        print("Opcion no valida")
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Simon Cifuentes Miranda
+##examen 1 de phyton
+### Medellin 2025
+####docente MR Gallego 
